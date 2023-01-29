4cb9c2da23d0e5fae4cde259ae7b10e98f0d8599
1576302867@qq.com <1576302867@qq.com>
2023-01-29 22:49:20 +0800

    完成目录界面

--- src/router/index.ts
+++ src/router/index.ts
@@ -7,14 +7,34 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: "/catalog",
+      path: "/index/index.html",
+      name: "index",
+      component: () => import("../views/Home.vue"),
+    },
+    {
+      path: "/",
+      name: "home",
+      component: () => import("../views/Home.vue"),
+    },
+    {
+      path: "/recommend/index.html",
+      name: "recommend",
+      component: () => import("../views/Recommend.vue"),
+    },
+    {
+      path: "/update/index.html",
+      name: "update",
+      component: () => import("../views/UpdatePage.vue"),
+    },
+    {
+      path: "/catalog/index.html",
       name: "catalog",
       component: () => import("../views/Catalog.vue"),
     },
     {
-      path: "/home",
-      name: "home",
-      component: () => import("../views/Home.vue"),
+      path: "/rank/index.html",
+      name: "rank",
+      component: () => import("../views/RankPage.vue"),
     },
     {
       path: "/about",
@@ -23,9 +43,10 @@ const router = createRouter({
     },
     {
       path: "/login",
-      name: "catalog",
+      name: "login",
       component: () => import("../views/login/LoginPage.vue"),
     },
+
     { path: "/users/:id", component: Activity },
     { path: "/login", component: LoginPage },
     // 将匹配所有内容并将其放在 `$route.params.pathMatch` 下
